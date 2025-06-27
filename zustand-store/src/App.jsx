614fd9f0bb218190e3b3212d1b50bb8be3ba63ae
MrNikhillYadav <facts.foundr@gmail.com>
2025-06-27 20:44:53 +0530

    fixed store

--- zustand-store/src/App.jsx
+++ zustand-store/src/App.jsx
@@ -11,9 +11,8 @@ function App() {
       <div>
         hello world
         <h1>Count : {count}</h1>
-        <button onClick={increment()}>Increment</button>
-        <button onClick={decrement()}>Decrement</button>
-
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
 
       </div>
     </>
