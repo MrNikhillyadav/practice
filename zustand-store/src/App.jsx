0d5b9de541be143cf01720a92b86fc72503db14d
MrNikhillYadav <facts.foundr@gmail.com>
2025-06-27 15:28:27 +0530

    add zustand-store dir

---
+++ zustand-store/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import useCountStore from './store'
+
+function App() {
+  const count = useCountStore((state) => state.count);
+  const increment = useCountStore((state) => state.increment);
+  const decrement = useCountStore((state) => state.decrement);
+  return (
+    <>
+      <div>
+        hello world
+        <h1>Count : {count}</h1>
+        <button onClick={increment()}>Increment</button>
+        <button onClick={decrement()}>Decrement</button>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
