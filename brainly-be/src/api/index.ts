3e66976a08117229e9837fb4ce59f626bcd9e312
MrNikhillYadav <facts.foundr@gmail.com>
2025-07-07 12:09:40 +0530

    add vercel.json

--- brainly-be/src/api/index.ts
+++ brainly-be/src/api/index.ts
@@ -10,7 +10,6 @@ import brainRouter from './routes/brain';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ||3000;
 
 app.use(express.json());
 app.use(cors());
@@ -20,10 +19,6 @@ app.use('/api/v1/content', authMiddleware , contentRouter)
 app.use('/api/v1/brain', authMiddleware , brainRouter)
 
 
-ConnectDB().then(() => {
+ConnectDB()
 
-    app.listen(PORT, () => {
-        console.log(`server is running on port ${PORT} `);
-    });
-
-});
+export default app;
